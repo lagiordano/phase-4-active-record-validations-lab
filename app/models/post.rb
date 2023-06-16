543d2ae82320d5a41b5e5b307b6e5f1cd958f254
Lauren Giordano <laurengiordano94@gmail.com>
2023-06-16 11:33:11 +1000

    done with lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,32 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :title_is_clickbait
+
+
+    #custom validations 
+    def title_is_clickbait
+        clickbait_regex = /\b(Won\'t Believe|Secret|Top \d+|Guess)\b/i
+        unless clickbait_regex.match? title
+            errors.add(:title, "This title is not clickbait-y enough!") 
+        end
+    end
+
+    # CLICKBAIT_PATTERNS = [
+    #     /Won't Believe/i,
+    #     /Secret/i,
+    #     /Top \d/i,
+    #     /Guess/i
+    #   ]
+    
+    #   def title_is_clickbait
+    #     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+    #       errors.add(:title, "must be clickbait")
+    #     end
+    #   end
+
+
 end
